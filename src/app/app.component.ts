fa6159ea753bb2486d1f170e8640acfe8383afe0
tom hikari <tomprescott1565@gmail.com>
2023-12-30 12:08:57 +0100

    fist comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponentComponent } from "./footer-component/footer-component.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponentComponent]
 })
 export class AppComponent {
   title = 'angular-first-app';
